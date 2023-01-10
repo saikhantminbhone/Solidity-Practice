6d6815a063539c58cf75fffb156ef34ff23ad672
saikhantminbhone <saikhantminbhone@gmail.com>
2023-01-10 23:03:12 +0630

    fist commmit

---
+++ contracts/fundMe/FundMe.sol
@@ -0,0 +1,26 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.8;
+
+import "./PriceConverter.sol";
+
+contract FundMe{
+
+    using PriceConverter for uint256;
+
+    uint256 public minimunUsd = 50 * 1e18;
+
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
+
+    function fund() public payable{
+        require(msg.value.getConversionRate() >= minimunUsd,"Didn't send enough");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
+    }
+
+    
+    // function withdraw(){
+
+    // }
+}
---
+++ contracts/fundMe/PriceConverter.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.8;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+
+library PriceConverter{
+    function getPrice() internal view returns(uint256){
+        //0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
+        (,int256 price,,,) = priceFeed.latestRoundData();
+        return uint256(price * 1e10);
+
+    }
+
+    function getConversionRate(uint256 ethAmount) internal view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+
+}
